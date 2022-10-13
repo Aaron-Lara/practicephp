fb3ca3f864a3c390d22a3859bd5c90f09b23abf8
Aaron-Lara <72263871+Aaron-Lara@users.noreply.github.com>
2022-10-13 09:58:01 -0500

    Add files via upload

---
+++ Carlos_Aaron_Lara_Medina/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+    <?php 
+    echo "Hello World! <br>";
+    Echo "Hello World! <br>";
+    ECHO "Hello World! <br>";
+    eChO "Hello World! <br>";
+
+    $color = "red";
+    echo "My car is ". $color. "<br>";
+    echo "My house is ". $COLOR . "<br>";
+    echo "My boat is ". $coLOR . "<br>";
+
+    $age = 13.555;
+    var_dump($age);
+    ?>
+    
+</body>
+</html>
