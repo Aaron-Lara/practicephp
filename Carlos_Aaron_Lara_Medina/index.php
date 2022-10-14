18663f364dd0789fb6c32eac2f4f2edf36c1c122
Aaron-Lara <72263871+Aaron-Lara@users.noreply.github.com>
2022-10-14 08:50:52 -0500

    Add files via upload

--- Carlos_Aaron_Lara_Medina/index.php
+++ Carlos_Aaron_Lara_Medina/index.php
@@ -9,6 +9,9 @@
 <body>
     <h1>My first PHP page</h1>
     <?php 
+    
+    //This is a single-line comment
+    #This is also a single-line comment
     echo "Hello World! <br>";
     Echo "Hello World! <br>";
     ECHO "Hello World! <br>";
@@ -18,9 +21,38 @@
     echo "My car is ". $color. "<br>";
     echo "My house is ". $COLOR . "<br>";
     echo "My boat is ". $coLOR . "<br>";
+    //The variables need to be write exactly as they were declare to work.
 
-    $age = 13.555;
-    var_dump($age);
+    //echo has no return value, take multiple parameters
+    echo "<h2>PHP is Fun!</h2>";
+    echo "Hello World! <br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+    print "<h2>PHP is Fun! </h2>";
+    print "Hello World!<br>";
+    print "I'm about to learn PHP!<br>";
+
+    // $age = 13.555;
+    // var_dump($age);
+
+    $grade = 32;
+    var_dump($grade);
+    echo "<br>";
+
+    if ($grade >= 60) {
+        echo "First Division";
+    } else if ($grade >= 45) {
+        echo "Second Division";
+    } else if ($grade >= 33) {
+        echo "Third Division";
+    } else{
+        echo "Failed";
+    } 
+    echo "<br>";
+
+    //echo ($grade >= 60) ? "First Division" : "Second Division";
+    
     ?>
     
 </body>
